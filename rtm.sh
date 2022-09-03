582e185da26a16e291e493655432cee53e0e2b55
sinabehmanesh <sina.behmanesh1999@gmail.com>
2022-09-03 16:29:00 +0100

    added colours to status output and reports

--- rtm.sh
+++ rtm.sh
@@ -11,7 +11,10 @@ datafile="./data"
 #variables for printing and grids
 bold_text=$(tput bold)
 normal_text=$(tput sgr0)
-
+green_text='\033[0;32m'
+blue_text='\033[0;34m'
+yellow_text='\033[0;33m'
+NC_text='\033[0m'
 
 #loggin function
 logger () {
@@ -44,7 +47,23 @@ if [ $# -eq 1 ] && [ $1 == "status" ]; then
     # while read line; do
     #     echo $line | grep -v "^$"
     # done < $datafile
-    column -t -s "|" --table-columns ID,TASK,STATUS data 
+    #column -t -s "|" --table-columns ID,TASK,STATUS data
+
+    printf  "%s \t\t" "ID"  "NAME" "STATUS"
+    printf "\n\n"
+    while read -r line ; do
+        #printf "${id}\t\t\t${name}\t\t\t${status}\n"  | grep -v "^$"
+        id=$(echo ${line} | cut -d '|' -f 1)
+        name=$(echo ${line} | cut -d '|' -f 2)
+        status=$(echo ${line} | cut -d '|' -f 3)
+        if [ ${status} == "TODO" ]; then
+            printf  "${id}\t\t${name}\t${yellow_text}${status}${NC_text} \n \n"  | grep -v "^$" 
+        elif [ ${status} == "DONE" ]; then
+            printf  "${id}\t\t${name}\t${green_text}${status}${NC_text} \n \n"  | grep -v "^$"
+        else
+            printf "No Tasks Found"
+        fi
+    done < $datafile
 
 #add tasks and insert them in data file
 elif [ $# -gt 1 ]; then
@@ -52,7 +71,7 @@ elif [ $# -gt 1 ]; then
         id=$(cat data | grep -v "^$" | wc -l )
         id=$(expr $id + 1)
         echo "${id} |${@: 2} | TODO " >> ${datafile}
-        printf "Task added with id ${id}. \n"
+        printf "Task added with id ${blue_text}${id}${NC_text}. \n"
     fi
 
 #to change the task status into DONE create a desired state of task and currentstate(todo) and replace the with sed
@@ -63,7 +82,7 @@ elif [ $# -gt 1 ]; then
 
         sed -i "s/^${currentstate}.*/${desiredstate} | DONE/" ${datafile}
 
-        printf "Task ${bold_text}${taskname}${normal_text} is now Done! good job lad! \n"
+        printf "Task ${blue_text}${bold_text}${taskname}${normal_text}${NC_text} is now ${green_text}Done!${NC_text} good job lad! \n"
         logger "${currentstate} changed to ${desiredstate} \n"
     fi
 
